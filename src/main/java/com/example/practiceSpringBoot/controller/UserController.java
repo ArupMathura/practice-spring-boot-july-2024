e2a7fa2af87cee71b3597ca79b88db804c956c85
ArupMathura <arupmathura06@gmail.com>
2024-07-30 10:48:01 +0530

    clear service and controller(do crud operation by using UserRepository)

--- src/main/java/com/example/practiceSpringBoot/controller/UserController.java
+++ src/main/java/com/example/practiceSpringBoot/controller/UserController.java
@@ -1,60 +1,12 @@
 package com.example.practiceSpringBoot.controller;
 
-import com.example.practiceSpringBoot.entity.User;
-import com.example.practiceSpringBoot.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 @Tag(name = "Rest Controller")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
-    @GetMapping
-    @Operation(summary = "Get user list")
-    public List<User> getUserList() {
-        return userService.getUserList();
-    }
-
-    @GetMapping("/{userId}")
-    @Operation(summary = "Get user by id")
-    public User getUserById(@PathVariable int userId) {
-        return userService.getUserById(userId);
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Post user")
-    public String addUser(@RequestBody User user) {
-        return userService.addUser(user);
-    }
-
-    @PutMapping("/{userId}")
-    @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "update user")
-    public User updateUser(@PathVariable int userId, @RequestBody User user) {
-        return userService.updateUser(userId, user);
-    }
-
-    @DeleteMapping("/{userId}")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "delete user")
-    public String deleteUser(@PathVariable int userId) {
-        return userService.deleteUser(userId);
-    }
-
-    @ExceptionHandler(RuntimeException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleRuntimeException(RuntimeException ex) {
-        return ex.getMessage();
-    }
 
 }
--- src/main/java/com/example/practiceSpringBoot/service/UserService.java
+++ src/main/java/com/example/practiceSpringBoot/service/UserService.java
@@ -1,90 +1,9 @@
 package com.example.practiceSpringBoot.service;
 
-import com.example.practiceSpringBoot.entity.User;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 @Service
 public class UserService {
-    List<User> userList = new ArrayList<>(
-            Arrays.asList(
-            new User(101, "arup", "mathura", "arup@gmail.com"),
-            new User(102, "riya", "das", "riya@gmail.com"),
-            new User(103, "ramu", "saha", "ramu@gmail.com")
-        )
-    );
 
-    /*---- get all users ----*/
-    public List<User> getUserList() {
-        return userList;
-    }
-
-    /*---- get user by id ----*/
-    /*public User getUserById(int userId) {
-        return userList.stream()
-                .filter(ref -> ref.getId()==userId)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("user with id " + userId + " not found"));
-    }*/
-
-    public User getUserById(int id) {
-        for (User user : userList) {
-            if (user.getId() == id) {
-                return user;
-            }
-        }
-        throw new RuntimeException("User with ID " + id + " not found");
-    }
-
-
-    /*---- Add user with id, else it will take default 0 as id ----*/
-    public String addUser(User user) {
-        userList.add(user);
-        return "User added successfully";
-    }
-
-    /*---- update user by id ----*/
-    public User updateUser(int userId, User updateUser) {
-        for (int i = 0; i < userList.size(); i++) {
-            User user = userList.get(i);
-            if (user.getId() == userId) {
-                user.setFirstName(updateUser.getFirstName());
-                user.setLastName(updateUser.getLastName());
-                user.setEmail(updateUser.getEmail());
-                return user;
-            }
-        }
-        throw new RuntimeException("User with id " + userId + " not found.");
-    }
-
-    /*---- delete user by id ----*/
-    /*public void deleteUser(int userId) {
-        for (int i=0; i<userList.size(); i++) {
-            User user = userList.get(i);
-            if (user.getId()==userId) {
-                userList.remove(i);
-//                return "User deleted successfully";
-                throw new RuntimeException("User deleted.");
-            }
-        }
-//        return "User id " + userId + " not found";
-        throw new RuntimeException("User with id " + userId + " not found.");
-    }*/
-
-    public String deleteUser(int id) {
-        Iterator<User> iterator = userList.iterator();
-        while (iterator.hasNext()) {
-            User user = iterator.next();
-            if (user.getId() == id) {
-                iterator.remove();
-                return "User deleted successfully";
-            }
-        }
-        throw new RuntimeException("User with ID " + id + " not found");
-    }
 
 }
